b6c56e0757d98b1ee7c1f1471cc714b0692cc1aa
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-04-02 20:18:39 +0300

    add new routes

--- app/routes/excelFile.routes.js
+++ app/routes/excelFile.routes.js
@@ -2,4 +2,6 @@ module.exports = (app) => {
    const createExcel = require("../controllers/createExcel.controllers.js");
 
    app.post("/api/createExcel", createExcel.createExcel);
+
+   app.post("/api/createBill", createExcel.createInvoice);
 };
